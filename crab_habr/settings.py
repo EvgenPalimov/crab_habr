695d66408cfa66e75603336b47890f3315d71a89
palim <palbimov@mail.ru>
2023-02-11 01:56:51 +0500

    edit fail settings.py v.9.26

--- crab_habr/settings.py
+++ crab_habr/settings.py
@@ -197,14 +197,14 @@ SUMMERNOTE_CONFIG = {
 
 # отправка электронной почты:
 DOMAIN_NAME = 'http://87.249.53.112'
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = 2525
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 # SERVER_EMAIL = EMAIL_HOST_USER
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
