3c321d137b41e23eb6729ca7d57a43ec40ba9dc7
palim <palbimov@mail.ru>
2023-02-10 19:37:03 +0500

    edit fail settings.py v.5

--- crab_habr/settings.py
+++ crab_habr/settings.py
@@ -196,13 +196,14 @@ SUMMERNOTE_CONFIG = {
 }
 
 # отправка электронной почты:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DOMAIN_NAME = 'http://85.193.80.140'
 EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
