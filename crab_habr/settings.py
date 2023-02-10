54df02a81046c49460b86ffecde5105dd3d6b9f6
palim <palbimov@mail.ru>
2023-02-10 20:20:55 +0500

    edit fail settings.py v.7

--- crab_habr/settings.py
+++ crab_habr/settings.py
@@ -210,38 +210,18 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        # Include the default Django email handler for errors
-        # This is what you'd get without configuring logging at all.
-        'mail_admins': {
-            'class': 'django.utils.log.AdminEmailHandler',
-            'level': 'ERROR',
-             # But the emails are plain text by default - HTML is nicer
-            'include_html': True,
-        },
-        # Log to a text file that can be rotated by logrotate
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
             'filename': (BASE_DIR / 'log/myapp.log')
         },
     },
     'loggers': {
-        # Again, default Django configuration to email unhandled exceptions
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        # Might as well log any errors anywhere else in Django
         'django': {
-            'handlers': ['logfile'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Your own app - this assumes all your logger names start with "myapp."
-        'crab_habr': {
-            'handlers': ['logfile'],
-            'level': 'INFO', # Or maybe INFO or DEBUG
-            'propagate': False
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
         },
     },
 }
+
